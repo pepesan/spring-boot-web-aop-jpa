627f2d5b1c24d01f901774476b776c7abac08105
David Vaquero <pepesan@gmail.com>
2022-07-10 19:46:52 +0200

    metidos diferentes llamadas con pointcuts

--- src/main/java/com/cursosdedesarrollo/aop/aspect/MiAspecto.java
+++ src/main/java/com/cursosdedesarrollo/aop/aspect/MiAspecto.java
@@ -5,6 +5,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -33,5 +35,21 @@ public class MiAspecto {
     @AfterThrowing(value="execution(* com.cursosdedesarrollo.aop.controllers.RestCrudController.getException(..))",throwing="ex")
     public void pillaExcepcion(JoinPoint joinPoint, Exception ex){
         log.info("Excepción en: "+ joinPoint.toString() + ": Excepción: "+ ex.getMessage());
+    }
+    // Seleccionamos todos los métodos del Controlador
+    @Pointcut("execution(* com.cursosdedesarrollo.aop.controllers.RestCrudController.*(..))")
+    public void selectAllControllerMethods(){}
+
+    @Before("selectAllControllerMethods()")
+    public void antesDeTodosControladores(JoinPoint joinPoint){
+        log.info("@Before: antesDeTodosControladores: " + joinPoint.toString());
+    }
+
+    // Seleccionamos todos los métodos de las clases de un paquete
+    @Pointcut("within(com.cursosdedesarrollo.aop..*)")
+    public void selectAll(){}
+    @Before("selectAll()")
+    public void antesDeTodo(JoinPoint joinPoint){
+        log.info("@Before: antesDeTodo: " + joinPoint.toString());
     }
 }
